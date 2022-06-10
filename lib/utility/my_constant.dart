9b7a1ffdc018283bfaa4e060836b7b7f836dc6d0
Hery <thitphavanh.psv@gmail.com>
2022-06-10 13:04:59 +0700

    update

---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  // General
+  static String appName = 'MarketPlace';
+
+  // Router
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = 'buyerService';
+  static String routeSalerService = 'salerService';
+  static String routeRiderService = 'riderService';
+}
