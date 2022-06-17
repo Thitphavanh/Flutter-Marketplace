8a6ecd38b45a61f56d7c6580c560d2f51d99b2d2
Hery <thitphavanh.psv@gmail.com>
2022-06-17 15:58:26 +0700

    update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_marketplace/utilities/my_constant.dart';
 import 'package:flutter_marketplace/widgets/show_image.dart';
+import 'package:flutter_marketplace/widgets/show_title.dart';
 
 class Authen extends StatefulWidget {
   const Authen({Key? key}) : super(key: key);
@@ -10,16 +11,124 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: size * 0.80,
-          child: ShowImage(pathImage: MyConstant.image1),
+        child: ListView(
+          children: [
+            const SizedBox(height: 30.0),
+            buildImage(size),
+            const SizedBox(height: 16.0),
+            buildAppName(),
+            const SizedBox(height: 10.0),
+            buildUser(size),
+            buildPassword(size),
+          ],
         ),
       ),
+    );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.9,
+          child: ShowImage(pathImage: MyConstant.image1),
+        ),
+      ],
+    );
+  }
+
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16.0),
+          padding: const EdgeInsets.only(left: 20.0),
+          width: size * 0.9,
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'User',
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              20.0,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(.5),
+                spreadRadius: 2.0,
+                blurRadius: 15.0,
+                offset: const Offset(0, 0),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 16.0),
+          padding: const EdgeInsets.only(left: 20.0),
+          width: size * 0.9,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Password',
+              icon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              20.0,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(.5),
+                spreadRadius: 2.0,
+                blurRadius: 15.0,
+                offset: const Offset(0, 0),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: MyConstant.appName,
+          textStyle: MyConstant().font1redstyle(),
+        ),
+      ],
     );
   }
 }
--- lib/utilities/my_constant.dart
+++ lib/utilities/my_constant.dart
@@ -17,4 +17,58 @@ class MyConstant {
   static String image3 = 'images/image3.jpg';
   static String image4 = 'images/image4.jpg';
   static String image5 = 'images/image5.jpg';
+
+  // Color
+  static Color primaryblue = const Color(0xff87ceeb);
+  static Color darkblue = const Color(0xff080742);
+  static Color lightlue = const Color(0xff1e90ff);
+
+  static Color primaryred = const Color(0xffd21502);
+  static Color darkred = const Color(0xff4c0805);
+  static Color lightred = const Color(0xff7f1019);
+
+  TextStyle font1style() => const TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle font2style() => const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle font3style() => const TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+      );
+
+  TextStyle font1redstyle() => TextStyle(
+        fontSize: 24,
+        color: darkred,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle font2redstyle() => TextStyle(
+        fontSize: 18,
+        color: darkred,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle font3redstyle() => TextStyle(
+        fontSize: 14,
+        color: darkred,
+        fontWeight: FontWeight.normal,
+      );
+
+  TextStyle font1bluestyle() => TextStyle(
+        fontSize: 24,
+        color: darkred,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle font2bluestyle() => TextStyle(
+        fontSize: 18,
+        color: darkred,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle font3bluestyle() => TextStyle(
+        fontSize: 14,
+        color: darkred,
+        fontWeight: FontWeight.normal,
+      );
 }
